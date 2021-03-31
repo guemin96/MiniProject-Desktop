c4d8eb0801c7ec82d997a2013ff105c1d1388aba
guemin96 <guemin12@naver.copm>
2021-03-31 17:19:12 +0900

    210331 커밋

--- WpfSMSApp/WpfSMSApp/View/User/UserList.xaml.cs
+++ WpfSMSApp/WpfSMSApp/View/User/UserList.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace WpfSMSApp.View.User
 {
@@ -27,23 +23,16 @@ namespace WpfSMSApp.View.User
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    var user = Commons.LOGINED_USER;
-            //    TxtUserID.Text = user.UserID.ToString();
-            //    TxtUserIdentityNumber.Text = user.UserIdentityNumber;
-            //    TxtUserSurName.Text = user.UserSurname;
-            //    TxtUserName.Text = user.UserName;
-            //    TxtUserEmail.Text = user.UserEmail;
-            //    TxtUserAdmin.Text = user.UserAdmin.ToString();
-            //    TxtUserActivated.Text = user.UserActivated.ToString();
+            try
+            {
+                RdoAll.IsChecked = true;
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    Commons.LOGGER.Error($"예외발생 MyAccount Loaded : {ex}");
-            //    throw ex;
-            //}
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 myaccount loaded : {ex}");
+                throw ex;
+            }
         }
 
         private void BtnEditAccount_Click(object sender, RoutedEventArgs e)
@@ -53,22 +42,145 @@ namespace WpfSMSApp.View.User
 
         private void BtnAddUser_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                NavigationService.Navigate(new AddUser());
 
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnAddUser_Click : {ex}");
+                throw ex;
+            }
         }
 
         private void BtnEditUser_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                NavigationService.Navigate(new EditUser());
 
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnEidtUser_Click : {ex}");
+                throw ex;
+            }
         }
 
         private void BtnDeactivateUser_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                NavigationService.Navigate(new DeactiveUser());
 
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnDeactivateUser_Click : {ex}");
+                throw ex;
+            }
         }
 
         private void BtnExportPdf_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "PDF File(*pdf)|*.pdf";
+            saveDialog.FileName = "";
+            if (saveDialog.ShowDialog() == true)
+            {
+                //PDF 변환
+                try
+                {
+                    iTextSharp.text.Font font = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12);
+                    string pdfFilePath = saveDialog.FileName;
 
+                    iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document(PageSize.A4);
+                    //PDF 객체 생성
+                    PdfPTable pdfTable = new PdfPTable(GrdData.Columns.Count);
+
+                    //PDF 내용 만들기
+                    string naumttf = Path.Combine(Environment.GetEnvironmentVariable("SystemRoot"), @"D2Coding.ttf");
+                    BaseFont nanumBase = BaseFont.CreateFont(naumttf, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                    var nanumFont = new iTextSharp.text.Font(nanumBase, 16f);
+
+                    Paragraph title = new Paragraph($"부경대 Stock Management System : {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+
+                    //PDF 파일생성
+                    using (FileStream stream = new FileStream(pdfFilePath,FileMode.OpenOrCreate))
+                    {
+                        PdfWriter.GetInstance(pdfDoc, stream);
+                        pdfDoc.Open();
+                        pdfDoc.Add(title);
+                        // 2번에서 만들 내용 추가
+                        pdfDoc.Close();
+                        stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Commons.LOGGER.Error($"예외발생 BtnExportPdf_Click : {ex}");
+                }
+            }
+            
+
+        }
+
+        private void RdoAll_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+
+                if (RdoAll.IsChecked ==true)
+                {
+                    users = Logic.DataAccess.GetUsers();
+                }
+                this.DataContext = users;
+                
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoActive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+
+                if (RdoActive.IsChecked == true)
+                {
+                    users = Logic.DataAccess.GetUsers().Where(u => u.UserActivated == true).ToList();
+                }
+                this.DataContext = users;
+
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+        }
+
+        private void RdoDeactive_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<WpfSMSApp.Model.User> users = new List<Model.User>();
+
+                if (RdoDeactive.IsChecked == true)
+                {
+                    users = Logic.DataAccess.GetUsers().Where(u => u.UserActivated ==false).ToList();
+                }
+                this.DataContext = users;
+
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
         }
     }
 }
